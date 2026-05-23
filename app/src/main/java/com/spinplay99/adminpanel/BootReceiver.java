14afd8a5eeb71467d6027dbaca6a0903734b62db
ujjwalrebel53-wq <ujjwalrebel53@gmail.com>
2026-05-23 20:55:16 +0530

    Update BootReceiver.java

--- app/src/main/java/com/spinplay99/adminpanel/BootReceiver.java
+++ app/src/main/java/com/spinplay99/adminpanel/BootReceiver.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.os.Build;
 
 public class BootReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (Intent.ACTION_BOOT_COMPLETED.equals(action) ||
-            "android.intent.action.QUICKBOOT_POWERON".equals(action)) {
-            
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
             Intent serviceIntent = new Intent(context, BackgroundSyncService.class);
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(serviceIntent);
             } else {
